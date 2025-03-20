0af967f1efb454b9da4601326ec8954e68fb775f
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-03-20 13:05:50 -0400

    created cdk project

--- lib/my-cdk-project-stack.ts
+++ lib/my-cdk-project-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create S3 bucket
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Only for dev/test environments
+    });    
   }
 }
